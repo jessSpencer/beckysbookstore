f52e200af375909f44bcd80badcdd62e74aa9615
Jessie Spencer <jeskspencer@gmail.com>
2022-02-21 18:42:54 -0700

    close

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using beckysbookstore.Models;
+using beckysbookstore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,13 +19,25 @@ namespace beckysbookstore.Controllers
         }
         public IActionResult Index(int pageNum = 1)
         {
-            int pageSize = 5;
-            var blah = repo.Books
+            int pageSize = 10;
+
+            var bub = new BooksViewModel
+            {
+                Books = repo.Books
                 .OrderBy(b => b.Title)
                 .Skip((pageNum - 1) * pageSize)
-                .Take(5);
+                .Take(pageSize),
 
-            return View(blah);
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+
+            return View(bub);
         }
     }
 }
--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using beckysbookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +14,41 @@ namespace beckysbookstore.Infrastructure
     [HtmlTargetElement("div", Attributes = "page-change")]
     public class PaginationTagHelper :TagHelper
     {
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
         
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        public PageInfo PageChange { get; set; }
+
+        public string PageAction { get; set; }
+        
+        public override void Process (TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i < PageChange.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
     }
 }
