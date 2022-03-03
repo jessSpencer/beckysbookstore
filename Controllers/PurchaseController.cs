c435e358d25c10dd40b0fd302597a54d22806a36
Jessie Spencer <jeskspencer@gmail.com>
2022-03-02 22:55:57 -0700

    done

---
+++ Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using beckysbookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beckysbookstore.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Purchase());
+        }
+    }
+}
