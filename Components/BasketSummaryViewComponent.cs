18eeee3517c198f4c6f22c18df8aef55c4e11541
Jessie Spencer <jeskspencer@gmail.com>
2022-03-05 17:32:36 -0700

    did  it!

---
+++ Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using beckysbookstore.Models;
+
+namespace beckysbookstore.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+            private Basket basket;
+            
+            public BasketSummaryViewComponent(Basket basketService)
+            {
+                basket = basketService;
+            }
+            public IViewComponentResult Invoke()
+            {
+                return View(basket);
+            }
+     }
+}
